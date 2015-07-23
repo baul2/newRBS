6ca87c3fee747b8b46ed75e8a4047b272606dc55
MulziSAW <MulziSAW@web.de>
2015-07-23 14:22:20 +0200

    Using of enums for ap1, ap2, dp1, dp2

--- newRBS_Console/newRBS_Console.cs
+++ newRBS_Console/newRBS_Console.cs
@@ -1,29 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace newRBS_Console
 {
     class newRBS_Console
     {
+
         static void Main(string[] args)
         {
-            CAEN_x730 cAENx730 = new CAEN_x730();
+            TraceSource trace = new TraceSource("newRBS_Console");
+            NamespaceCAEN_x730.CAEN_x730 cAENx730 = new NamespaceCAEN_x730.CAEN_x730();
             cAENx730.StartAcquisition(0);
             System.Threading.Thread.Sleep(2000);
-            UInt32[] hist = cAENx730.GetHistogram(0);
+            uint[] hist = cAENx730.GetHistogram(0);
             int Sum = 0;
             for (int i = 0; i < hist.Length; i++)
             { Sum += (int)hist[i]; }
             Console.WriteLine(Sum);
 
-            //Test
-            //a
-            //Logger.Info("Measurement running", "Main");
-            //Logger.Error("sdf", "Main");
-
+            trace.TraceEvent(TraceEventType.Information, 0, "{0} counts", Sum);
 
             cAENx730.StopAcquisition(0);
             cAENx730.Close();
